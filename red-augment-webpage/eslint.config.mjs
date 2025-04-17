04c093c0db5bf4df03d538e6c17bc34fae097753
jithingeorgedev <jithin.george.el@gmail.com>
2025-04-18 01:35:28 +0530

    commit

--- red-augment-webpage/eslint.config.mjs
+++ red-augment-webpage/eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off', // Disable the no-explicit-any rule
+      '@typescript-eslint/no-unused-vars': 'off',  // Disable unused-vars check for TypeScript
+      '@next/next/no-img-element': 'off',        // Disable warning for using <img> instead of <Image />
     },
   },
 ];
